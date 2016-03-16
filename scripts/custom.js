c2066b1164c08594575bca29da154e9df0fa2845
Flor Antara <florenciaantara@gmail.com>
2016-03-16 13:25:57 -0700

    Fixed: popup not showing on Give Today page if url ends with /.

--- scripts/custom.js
+++ scripts/custom.js
@@ -15,8 +15,12 @@
 
 	var injectPopup = function () {
 		var href = window.location.href;
-		var thisPage = href.split('/').pop();;
-		if (thisPage == "give"){
+		var thisPage = href.split('/').pop();
+
+		var tarr = href.split('/'); //
+		var thisPageWithSlash = tarr[tarr.length-2];
+
+		if (thisPage == "give" || thisPageWithSlash == "give"){
 			$("body").prepend("<div class='overlay'></div><div id='donate-popup'><div class='close-popup'>X</div><iframe src='https://entry.donorsnap.com/forms/oForms.aspx?id=NTI3YjQwOWYtZWYyNi00NTVmLWJiNWMtYzY1ZGI0MWEzZGI3'></iframe</div>");
 		}
 	}
